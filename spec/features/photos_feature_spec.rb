86fc60a77d82ccaf247b4edd54ef165632d0f96e
Tatiana Soukiassian <binaryberry@gmail.com>
2014-12-20 18:15:06 +0100

    posts can now be added but only as texts, not images

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -8,4 +8,16 @@ describe 'photos' do
             expect(page).to have_link 'Add a photo'
         end
     end
+
+    context 'photos have been added' do
+	    before do
+	        Photo.create(caption: 'Panda')
+	    end
+
+	    it 'should display photos' do
+	        visit '/photos'
+	        expect(page).to have_content('Panda')
+	        expect(page).not_to have_content('No photos yet')
+	    end
+	end
 end
